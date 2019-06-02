97769c88767a4d6c14fc40dd02b2402a4bd98a08
nathanbud <nthnlbdky916@gmail.com>
2019-06-02 22:48:38 +0800

    first commit

---
+++ rsample_1/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import productData from './productData'
+import Product from './productData';
+
+const App = () =>{
+    products.map(product=>{
+        <Product product={product}/>
+    });
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default App
