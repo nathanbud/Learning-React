cad75efa509a81d3fbc8e38ecfcf321080383219
nathanbud <nthnlbdky916@gmail.com>
2019-09-09 10:43:32 +0800

    Learning useEffect

--- hooks/learnhooks/src/App.js
+++ hooks/learnhooks/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button'
+import UseEffectExample from './components/effectUse'
 
 function App() {
   return (
     <div className="App">
-      <Button/>
+      {/* <Button/> */}
+      <UseEffectExample/>
     </div>
   );
 }
